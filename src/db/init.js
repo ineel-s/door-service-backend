e5d46ea089707931bdc3d0693b953df498644ce5
Neelmani Shukla <ssprshivamshukla@gmail.com>
2023-01-05 16:01:13 +0530

    Added frontend + backend tgether

--- src/db/init.js
+++ src/db/init.js
@@ -2,22 +2,28 @@ const mongoose = require( 'mongoose' );
 
 mongoose.set('returnOriginal',false);
 mongoose.set('runValidators', true);
-
+mongoose.set('strictQuery', true);
 require('../models/User');
 require('../models/ManageService');
 require('../models/Servicecategory');
 require('../models/service');
 
 const {
+    NODE_ENV,
     DB_HOST,
     DB_PORT,
-    DB_NAME
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD
 } = process.env;
+
+const connectionStr = NODE_ENV ==='development'? `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`:
+`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 
 const connect = async ()=>{
     try {
-        await mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`);
+        await mongoose.connect(connectionStr);
         console.log('connected to db');      
     } catch (error) {
         console.log(error.message);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 JWT_SECRET = require('dotenv').config();
 
+const path = require('path')
 const express = require('express');
-const cors = require('cors')
-const { connect } = require('./db/init');
 const app = express();
+
+
+const cors = require('cors')
+app.use( express.static(path.join(process.cwd(), './public/dist')))
+const { connect } = require('./db/init');
 app.set('app-title', 'Door Service');
 
 const PORT = process.env.SERVER_PORT || 3000;
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-app.use( cors({
-    origin: ['http://localhost:3001' || 'http://192.168.2.17:3001'],
-}));
+if(process.env.NODE_ENV==='development'){
+app.use( cors());
+}
+
+
 app.use('/auth',require('./routes/auth.routes'));
 
 app.use('/category', require('./routes/serviceCategory.routes'));
@@ -23,6 +30,9 @@ app.use('/user',require('./routes/user.routes'));
 
 app.use('/bookingservice',require('./routes/manageservice.routes'));
 
+app.use(function(req,res,next){
+    res.sendFile(path.join(process.cwd(), './public/dist', 'index.html'));
+});
 
 connect()
     .then(()=>{
